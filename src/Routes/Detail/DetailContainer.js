b1bbcd3398d9b217210a9c1e18040401d995b49f
Jusang You <everflavor@gmail.com>
2021-01-06 23:22:22 -0500

    Setting request data by page id

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,15 +1,56 @@
 import React from 'react';
+import { tvApi } from '../../api';
+import { movieApi } from '../../api';
 import DetailPresenter from './DetailPresenter';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes('/movie'),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push('/');
+    }
+
+    let result = null;
+
+    try {
+      if (isMovie) {
+        const request = await movieApi.movieDetail();
+        result = request.data;
+      } else {
+        const request = await tvApi.showDetail(parsedId);
+        result = request.data;
+      }
+    } catch {
+      this.setState({ error: "Can't find anything!" });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
 
   render() {
+    console.log('this.props:', this.props);
+    console.log('this.state:', this.state);
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
